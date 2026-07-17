28cea297eced57e615d0ffa1b0b5b8e4d21cdb06
Tim <79818585+TimmieDevelops@users.noreply.github.com>
2026-07-17 14:47:26 -0500

    [+} TODO: IDADumper

--- FortGen/dllmain.cpp
+++ FortGen/dllmain.cpp
@@ -10,19 +10,6 @@ DWORD MainThread(HMODULE Module)
     Address::SetupOffsets();
     // GUObjectArray = reinterpret_cast<FUObjectArray*>(Scanner::GetModuleBase() + Address::GUObjectArray);
     GUObjectArray = decltype(GUObjectArray)(Scanner::GetModuleBase() + Address::GUObjectArray);
-    Logger::Log(LogLevel::Info, std::format("NumElements={}", GUObjectArray->GetObjObjects().GetNumElements()).c_str());
-    for (int i = 0; i < GUObjectArray->GetObjObjects().GetNumElements(); i++)
-    {
-        Logger::Log(LogLevel::Info, std::format("fmgnio gay={}", i).c_str());
-        UObjectBase* Object = GUObjectArray->GetObjObjects().GetObjects(i)->GetObjectW();
-        if (!Object)
-        {
-            Logger::Log(LogLevel::Info, "Object is null!");
-            continue;
-        }
-
-        Logger::Log(LogLevel::Info, Object->GetNamePrivate().ToString());
-    }
     return 0;
 }
 
